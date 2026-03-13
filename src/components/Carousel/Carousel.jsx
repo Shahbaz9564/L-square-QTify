44780b50e93cf2192fac562409c2bedb6ff11235
Md shahbaz <mdserajuddin9955@gmail.com>
2026-03-14 01:35:45 +0530

    Fix carousel slider test

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,39 +1,41 @@
 //import React, { useEffect, useRef, useState } from 'react';
 // Import Swiper React components
-import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
+import { Swiper, SwiperSlide, useSwiper } from 'swiper/react';
 
 // Import Swiper styles
-import "swiper/css";
-import "swiper/css/navigation";
-import LeftNavigation from "./LeftNavigation";
-import RightNavigation from "./RightNavigation";
+import 'swiper/css';
+import 'swiper/css/navigation';
+import LeftNavigation from './LeftNavigation';
+import RightNavigation from './RightNavigation';
 import styles from "./Carousel.module.css";
 
 // import required modules
-import { Navigation } from "swiper/modules";
+import { Navigation } from 'swiper/modules';
 
-const Control = ({ data }) => {
+const Control =({data}) =>{
   const swiper = useSwiper();
-};
 
-export default function Carousel({ data, renderComponent }) {
+  
+}
+
+export default function Carousel({data, renderComponent}) {
+
+
   return (
     <div>
-      <Swiper
-        navigation={true}
-        modules={[Navigation]}
-        className="mySwiper"
-        slidesPerView={7}
-        spaceBetween={40}
-      >
-        <Control data={data} />
+      <Swiper navigation={true} modules={[Navigation]} className="mySwiper" slidesPerView={"auto"} spaceBetween={40} >
+        <Control data ={data} />
         <div>
           <LeftNavigation />
           <RightNavigation />
         </div>
-        {data.map((ele) => (
-          <SwiperSlide>{renderComponent(ele)}</SwiperSlide>
-        ))}
+        {data.map((ele)=>(
+<SwiperSlide>
+  
+  {renderComponent(ele)}</SwiperSlide>
+        )
+    )
+}
       </Swiper>
     </div>
   );
